846ba1d44942697d3b552f9c25bdb66829da5198
王郑 <418706636@qq.com>
2019-02-26 11:13:25 +0800

    add

---
+++ DistributedLock/DistributedLock.cs
@@ -0,0 +1,17 @@
+﻿using org.apache.zookeeper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistributedLock
+{
+    public class DistributedLock
+    {
+
+        public void Test()
+        {
+            var zooKeeper =new ZooKeeper("118.24.96.212",50000,)
+        }
+    }
+}
---
+++ DistributedLock/MyWatch.cs
@@ -0,0 +1,16 @@
+﻿using org.apache.zookeeper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistributedLock
+{
+    public class MyWatch : Watcher
+    {
+        public override Task process(WatchedEvent @event)
+        {
+            return null;
+        }
+    }
+}
